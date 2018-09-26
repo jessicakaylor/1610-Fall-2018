7353c39503d4be051ae6b023a35a1def6597a804
jessicakaylor <42555633+jessicakaylor@users.noreply.github.com>
2018-09-26 00:32:59 -0600

    Add files via upload

---
+++ Jessicas project/Assets/Scenes/Scenes/Variables.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+	public bool IsAWizard;
+
+	public int AmmoCount = 100;
+	public int InventoryCount;
+
+	public float Health = 1.0f;
+	public float Money;
+
+	public string PlayerName;
+	public string Team = "Wolves";
+
+	public Vector3 Location;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		print(PlayerName);
+		print("has");
+		print("$" + Money);
+
+		if (IsAWizard)
+		{
+			print(PlayerName + "Is A Wizard!!!");
+		}
+		else
+		{
+			print(PlayerName + "Is A Jedi!");
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		transform.position = Location;
+
+	}
+}
