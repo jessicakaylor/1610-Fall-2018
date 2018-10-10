ea6e5b976df39fa090d2d6bbad7215c0c5904381
jessicakaylor <42555633+jessicakaylor@users.noreply.github.com>
2018-10-10 15:36:48 -0600

    Add files via upload

---
+++ Jessicas project/Assets/Localvars.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class Localvars : MonoBehaviour
+{
+
+    public string Name;
+
+    public void Call()
+    {
+        Name = "Bob";
+        print(name);
+    }
+
+    public void Call(string newName)
+    {
+         print(newName);
+        print(Name);
+    }
+    
+}
---
+++ Jessicas project/Assets/WhatCanSeeIt.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatCanSeeIt : ScriptableObject
+{
+
+	private float health;
+	protected float PowerLevel;
+	public float EnergyLevel;
+
+	private void AddHealth()
+	{
+		health = 100;
+	}
+
+	protected void AddPower()
+	{
+
+	}
+
+	public void AddEnergy()
+	{
+
+	}
+}
+
+public class ICanSeeIt : MonoBehaviour
+{
+	private WhatCanSeeIt newobject;
+
+	private void Start()
+	{
+		newobject.EnergyLevel = 100;
+		newobject.AddEnergy();
+	}
+}
+
+public class Child : WhatCanSeeIt()
+{
+	void CallThings()
+	{
+		PowerLevel = 200;
+		EnergyLevel = 100;
+		AddPower();
+		AddEnergy();
+	}
+}
+
+
+
