ac1aa661ab49119c04751d960d860de089607074
jessicakaylor <42555633+jessicakaylor@users.noreply.github.com>
2018-11-06 23:37:16 -0700

    Add files via upload

---
+++ Jessicas project/Assets/AI/BasicAi.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicAi : MonoBehaviour
+{
+	private NavMeshAgent agent;
+	public Transform Destination;
+	
+
+	// Use this for initialization
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Destination != null) agent.destination = Destination.position;
+	}
+}
