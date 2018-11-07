26a7fddff4a6fd17b87e9644cedcf85bb582075a
jessicakaylor <42555633+jessicakaylor@users.noreply.github.com>
2018-11-07 15:24:27 -0700

    Add files via upload

---
+++ Jessicas project/Assets/EventRepeat.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventRepeat : MonoBehaviour
+{
+
+	public UnityEvent[] OnRepeat;
+	
+	public float Seconds = 2;
+	private int i = 0;
+	
+	void Start ()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(2);
+			if (i < OnRepeat.Length-1)
+			{
+				i++;
+			}
+			else
+			{
+				i = 0;
+			}
+		
+		
+			
+		}
+	}
+	
+}
