c3ce52cc617ea3030a8689bf54bd6f19ffdd91a5
jessicakaylor <42555633+jessicakaylor@users.noreply.github.com>
2018-12-07 22:03:45 -0700

    Add files via upload

---
+++ New Unity Project/Assets/Scripts/OnCollisionDestroyObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionDestroyObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
+		//Debug.Log(collision.collider.name);
+		if (collision.gameObject.name == "rey")
+		{
+			Destroy(gameObject);
+		}	
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New Unity Project/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+   
+    public CharacterController Controller;
+
+    public float Gravity = 9.81f;
+    public float JumpSpeed = 7.0f;
+    public float MoveSpeed = 5.0f;
+    public float FlipSpeed = 5.0f;
+
+    private Vector3 location;
+    private Vector3 rotation;
+
+    //private Vector3 rotation;
+    // Use this for initialization
+    void Start()
+    {
+        Controller = GetComponent< CharacterController >();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {  
+        location.x = MoveSpeed * Input.GetAxis( "Horizontal" );
+        location = transform.TransformDirection( location );
+       
+       // transform.Rotate(0, Input.GetAxis("Horizontal")*FlipSpeed*Time.deltaTime, 0);        
+
+        if ( Controller.isGrounded )
+        {
+            location.y = JumpSpeed * Input.GetAxis("Vertical");
+
+            if ( Input.GetButton( "Jump" ) )
+            {
+                location.y = JumpSpeed;
+            }
+        }
+        
+        location.y -= Gravity * Time.deltaTime;
+        Controller.Move( location * Time.deltaTime );
+    }
+}
