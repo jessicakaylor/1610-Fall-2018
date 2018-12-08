e00dbfef70238b8ab71c55f71c2fe7708ebb928c
jessicakaylor <42555633+jessicakaylor@users.noreply.github.com>
2018-12-07 22:01:43 -0700

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public CharacterController Character;
+    public float Speed;
+    public Vector3 MoveSpeed;
+	
+	
+    private void Update()
+    {
+        MoveSpeed.x += Speed*Input.GetAxis("Horizontal");
+        MoveSpeed *= Time.deltaTime;
+        MoveSpeed.y += Input.GetAxis("Vertical");
+        Character.Move(MoveSpeed);
+    }
+}
