45522fa417cd6ffc203abbc030d62d7de316cf71
jessicakaylor <42555633+jessicakaylor@users.noreply.github.com>
2018-12-10 16:03:58 -0700

    Add files via upload

---
+++ DestroyButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DestroyButton : MonoBehaviour
+{
+	// Use this for initialization
+
+
+	public GameObject objectToDestroy;
+	public void DestroyGameObject()
+	{	
+		
+		print("FIND LUKE AT THE TOP BUT WATCH OUT FOR PORGS!");
+			Destroy(objectToDestroy);
+	}
+
+}
---
+++ InstantiateObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+	public GameObject BlasterShotPrefab = null;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+
+	{
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+
+		{
+			Instantiate(
+				BlasterShotPrefab,
+				gameObject.transform.position,
+				Quaternion.identity);
+			{
+				//if (Input.GetKeyDown(KeyCode.DownArrow))
+
+				{
+					//Instantiate(
+					//BlasterShotPrefab,
+					//gameObject.position,
+					//Quaternion.identity);
+				}
+			}
+		}
+	}
+}
